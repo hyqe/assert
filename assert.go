93486fde69718e3399241352bd6d0d469ff4b773
Jacob Ochoa <jacob.ochoa@gmail.com>
2022-12-06 12:46:12 -0800

    base comparison tools

---
+++ assert.go
@@ -0,0 +1,42 @@
+package assert
+
+import "testing"
+
+func Eq[T comparable](t *testing.T, a, b T) {
+	if a == b {
+		return
+	}
+	t.Fatalf("%v != %v", a, b)
+}
+
+type Number interface {
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64
+}
+
+func GT[T Number](t *testing.T, a, b T) {
+	if a > b {
+		return
+	}
+	t.Fatalf("%v < %v", a, b)
+}
+
+func LT[T Number](t *testing.T, a, b T) {
+	if a < b {
+		return
+	}
+	t.Fatalf("%v > %v", a, b)
+}
+
+func GE[T Number](t *testing.T, a, b T) {
+	if a >= b {
+		return
+	}
+	t.Fatalf("%v < %v", a, b)
+}
+
+func LE[T Number](t *testing.T, a, b T) {
+	if a <= b {
+		return
+	}
+	t.Fatalf("%v > %v", a, b)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hyqe/assert
+
+go 1.19
